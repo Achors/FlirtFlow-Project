62c98161ea702826bea425e6021d07cda85816ea
izzie <izzienjeri12@gmail.com>
2023-11-08 22:49:47 +0300

    upated file name

--- flirtflow/src/App.js
+++ flirtflow/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RootLayout from "./Layout/RootLayout";
 import Home from "./Components/Home";
+import UserData from "./Components/UserData";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<RootLayout />}>
             <Route path="/home" element={<Home />} />
+            <Route path= "/UserData" element= {<UserData />} />
           </Route>
         </Routes>
       </BrowserRouter>
